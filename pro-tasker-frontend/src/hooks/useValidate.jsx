46f3556722d009801eb56f42386565c4dd5aa41d
Mithra <mithra29.venu@gmail.com>
2026-04-01 11:19:56 -0400

    added useValidate

---
+++ pro-tasker-frontend/src/hooks/useValidate.jsx
@@ -0,0 +1,60 @@
+//validation for register user
+export const useValidateRegister = (form) => {
+  let errors = {};
+
+  if (!form.firstName.trim()) {
+    errors.firstName = "First name is required";
+  }
+
+  if (!form.lastName.trim()) {
+    errors.lastName = "Last name is required";
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!form.email) {
+    errors.email = "Email is required";
+  } else if (!emailPattern.test(form.email)) {
+    errors.email = "Invalid email";
+  }
+
+  const passwordPattern =
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+
+  if (!form.password) {
+    errors.password = "Password is required";
+  } else if (!passwordPattern.test(form.password)) {
+    errors.password =
+      "Must be 8+ chars, include uppercase, lowercase, and number";
+  }
+
+  if (form.password !== form.confirmPassword) {
+    errors.confirmPassword = "Passwords do not match";
+  }
+
+  return errors;
+};
+
+//validation for login user
+export const useValidateLogin = (form) => {
+  let errors = {};
+
+  // Email validation
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!form.email) {
+    errors.email = "Email is required";
+  } else if (!emailPattern.test(form.email)) {
+    errors.email = "Invalid email format";
+  }
+
+  // Password validation
+  if (!form.password) {
+    errors.password = "Password is required";
+  } 
+ // Don't enforce strong rules here since user already registered
+  else if (form.password.length < 8) {
+    errors.password = "Password must be at least 8 characters";
+  }
+
+  return errors;
+};
