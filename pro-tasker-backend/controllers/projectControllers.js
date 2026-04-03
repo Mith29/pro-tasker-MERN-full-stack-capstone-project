2fa11803e175166e1d68476a50757f1b62e7ce8d
Mithra <mithra29.venu@gmail.com>
2026-04-03 10:21:20 -0400

    updated importing filenames

--- pro-tasker-backend/controllers/projectControllers.js
+++ pro-tasker-backend/controllers/projectControllers.js
@@ -1,5 +1,5 @@
-import Project from "../models/Project.js";
-import Task from "../models/Task.js";
+import Project from "../models/project.js";
+import Task from "../models/task.js";
 //create project
 export async function createProject(req, res) {
   try {
